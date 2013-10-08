e122be3ec73115d9ff0ae8df9d314926b048ef1f
dan tsai <tsai.dan@gmail.com>
2013-10-08 14:43:17 -0700

    basic js

--- tracker.js
+++ tracker.js
@@ -1,4 +1,4 @@
-// [kate, dan] get all the review divs
+// [kate] get all the review divs
 
 // [kate, dan] add buttons for Food, Service, Atmosphere to each review div
 
@@ -9,7 +9,7 @@
 // if this is not possible, we might have to do a database and write a script to post to it, and also return json output when queried.
 
 
-// [kate, dan]
+// [dan]
 // add filtering/sorting options at the top of the review page
 // "Sort by: Yelp Sort [...]  Food, Service, Atmosphere"
 
@@ -17,13 +17,4 @@
 // need to have some way to detect that the user is trying to sort by a custom extension-based sort type
 // if that's the case, then display the reviews in the appropriate order
 
-document.addEventListener('DOMContentLoaded', function () {
-	alert("click test1");
-	$('#button').click(function() {
-		console.log("click test2");
-		// chrome.tabs.query ({active: true, currentWindow: true}, function(tab) {
-		// bookmarkIt(tab);
-		// })
-		event.preventDefault();
-	});
-});
+ $("#review_sort_section").append('Test');
