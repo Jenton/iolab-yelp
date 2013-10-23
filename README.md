8caae514959e07b04480ad5eff0d9239866451f3
dan tsai <tsai.dan@gmail.com>
2013-10-22 18:12:42 -0700

    writeareview page

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 <h3>Project Description</h3>
 <p>Currently, the only tags you can add to a Yelp review are 'Useful', 'Funny' and 'Cool'. We feel that these tags provide useless metadata to a review (and even incentivize reviewers to generate "funny" content that may not necessarily be valuable other than for comedic relief). Our project changes those tags to ones that help classify reviews based on whether they are about food, about the service, the atmosphere, and/or the price of the restaurant. Our project also allows users to sort a restaurant's Yelp reviews based on those tags. So if a user wanted to view a restaurant's reviews that were more focused on the food, he or she could do that now. Currently, Yelp has no function that allows you to do this.
+<p>As a proof of concept, we also modified the Write a Review page to automatically tag a review as it is written, by analyzing the text. Unfortunately, because we do not know the ID of a review before it is submitted, we cannot actually do this -- it would have to be done on the backend.</p>
 
 <h4>Metacrap Justification</h4><p>We believe that the current Yelp review tags are biasing the way Yelp users write their reviews. Instead of writing a review that actually gives helpful information to other users about a restaurant, we feel that many reviews are written solely in order to get a 'Funny' or a 'Cool' tag from other users. The tags that Yelp are providing are a type of metric that is influencing the results, as seen in the user reviews. This creates a body of reviews that is strongly influenced by the metrics provided to judge those reviews, as suggested by Doctorow's 6th obstacle ("metrics influence results"). We believe that creating metrics/tags that consumers are actually interested in will create a more useful corpus of reviews.</p>
 
@@ -14,10 +15,10 @@ Dan Tsai - Front End/Chrome Extension<br />
 Kate Hsaio - Front End/Chrome Extension</p>
 
 <h3>Technologies Used</h3>
-<p>HTML/CSS/Javscript for the Chrome Extension, MySQL/PHP for the database</p>
+<p>HTML/CSS/Javscript/jQuery for the Chrome Extension, MySQL/PHP for the database</p>
 
 <h3>Link to Demo Version</h3>
-<p>pending packaged .crx file...</p>
+<p></p>
 
 <h3>Known Bugs</h3>
 <p>The extension currently only sorts by the current page of reviews. This is because Yelp provides no way to get all reviews associated with a business (even with the API, as far as we can tell).</p> 
---
+++ yelp-tagger-add.js
@@ -0,0 +1,85 @@
+// proof of concept to automatically tag a review when it's being written/added.
+// We can't actually implement this because we don't know the reviewID of a review prior to its submission
+// if we were yelp and could do this on the backend, that would be a different story
+$(document).ready(function(){
+	// Get business id. Last segment of the URL (split off beginning part)
+	// but without query string (so split off the last part)
+	var currentURL = document.URL;
+	// alert("currentURL: " + currentURL);
+	var urlWithoutQueryString = currentURL.split('?')[0];
+	// alert("urlWithoutQueryString: " + urlWithoutQueryString);
+	var bizId = urlWithoutQueryString.substr(urlWithoutQueryString.lastIndexOf('/') + 1);
+	alert("bizID: " + bizId);
+
+
+	$("#save_now").on("click", function() {
+		haskey();
+	});
+
+
+	function haskey() {
+	    var total_food = 0;
+	    var total_service = 0;
+	    var total_atmosphere = 0;
+	    var total_price = 0;
+	    var review_str =$("#review-text").val().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase().split(" ");
+	    var food_list = ["", "juicy", "fresh", "seasoned", "salty", "dessert", "milk", "sugar", "water", "salad", "culinary", "coffee", "pizza", "pasta", "food", "delicious", "appetizing", "crunchy", "spicy", "sweet", "greasy", "fishy", "buttery", "bland", "bitter", "aftertaste", "mouthwatering", "flavors", "flavor"];
+	    var service_list = ["", "professional", "reservation", "personal", "reservation", "staff", "service", "slow", "pleasure", "polite", "waiter", "waitress", "fast", "helpful", "friendly", "prompt", "efficient"];
+	    var atmosphere_list = ["", "crowd","parties", "party","ambiance", "anniversary", "light", "music", "conversation", "fireplace", "loud", "quite"];
+	    var price_list = ["", "cheap", "expensive", "price", "cost", "discounted", "low-priced"];
+	    
+	    //create an empty dict. 
+	    var haskey_dict = {};
+
+	    //iterate over each of the items in the review_str and place "word:1" into dict. 
+	    //if word exists already word+=1.
+	    for (var i = 0; i < review_str.length; i++) {
+	      if (review_str[i] in haskey_dict) {
+	        haskey_dict[review_str[i]] += 1;
+	      }
+	      else {
+	        haskey_dict[review_str[i]] = 1;
+	      }
+	    };
+	    console.log(haskey_dict)
+	    $.each(haskey_dict, function(key, value) { 
+	        contains_food = $.inArray(key, food_list);
+	        if (contains_food > 0)
+	          total_food += value
+
+	        contains_service = $.inArray(key, service_list);
+	        if (contains_service > 0)
+	          total_service += value
+	        
+	        contains_atmosphere = $.inArray(key, atmosphere_list);
+	        if (contains_atmosphere > 0)
+	          total_atmosphere += value
+	        
+	        contains_price = $.inArray(key, price_list);
+	        if (contains_price > 0)
+	          total_price += value
+	    });
+
+	    // if any metric rating is above 3, let's add a tag
+	    var alertStr = "Tag review with: ";
+	    if(total_food >= 3)
+	    	alertStr += "Food ";
+	    if(total_service >= 3)
+	    	alertStr += "Service ";
+	    if(total_atmosphere >= 3)
+	    	alertStr += "Atmosphere ";
+	    if(total_price >= 3)
+	    	alertStr += "Price ";
+	    alert(alertStr);
+
+
+		// $.ajax({
+  //   		type:"post",
+  //   		url:"http://people.ischool.berkeley.edu/~jenton/IO_Lab_P2/phpScript.php",
+  //   		data:"action=service"+"&bizID="+bizId+"&reviewID="+selectedId+"&reviewIDs="+idArray,
+  //   		success:function(data){
+  //    			updateCounts(data);
+  //  			}
+ 	// 	});
+	};
+});
